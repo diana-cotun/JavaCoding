e46a1eedc1df2362cb01aa4667f2888a60ed2343
Diana Cotun <diana.cotun@gmail.com>
2023-10-19 21:13:30 +0300

    numereimpare

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,36 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator(5, 10);
-        calculator.setTermenUnu(2.3f);
+        calculator.setTermenUnu(2);
         calculator.setTermenDoi(5);
 
 //        System.out.println(calculator.adunare());
 //        System.out.println(calculator.scadere());
 
         Scanner scaner = new Scanner(System.in);
-        String operatii = scaner.next().toUpperCase();
+
+//        String operatii = scaner.next().toUpperCase();
+//        if (Operatii.ADUNARE.toString().equals(operatii)){
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.toString().equals(operatii)){
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea introdusa nu este valida");
+//        }
+
+//        System.out.println("Introduceti codul operatiei dorite: 1 sau 2");
+//        int cod = scaner.nextInt();
+//        if (Operatii.ADUNARE.getCod() == cod){
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.getCod() == cod) {
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea nu este valida");
+//        }
+
+        System.out.println(calculator.numereImpare());
 
 
-        if (Operatii.ADUNARE.toString().equals(operatii)){
-            System.out.println(calculator.adunare());
-        } else if (Operatii.SCADERE.toString().equals(operatii)){
-            System.out.println(calculator.scadere());
-        } else {
-            System.out.println("Valoarea introdusa nu este valida");
-        }
 
 
 
